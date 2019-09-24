f31a74023cb92188132cc73c634fc16f98d16a6e
Alan Tang <alantang888@users.noreply.github.com>
2019-09-24 22:31:26 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='stackdriver_json_formatter',
+    version='',
+    packages=['stackdriver_json_formatter'],
+    url='',
+    license='',
+    author='Alan Tang',
+    author_email='',
+    description=''
+)
